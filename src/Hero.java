fb73435e655a444854f653629be7263571832c5b
Unknown <oblipn@gmail.com>
2018-03-06 11:38:54 +0100

    1.3

---
+++ src/Hero.java
@@ -0,0 +1,54 @@
+public class Hero {
+    private String name;
+    private int life;
+    private int maxLife;
+    private int stamina;
+    private int maxStamina;
+
+    public Hero(String name){
+        this.name = name;
+    }
+    public Hero(){
+        this.name = "Ynovator";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getMaxLife() {
+        return maxLife;
+    }
+
+    public void setMaxLife(int maxLife) {
+        this.maxLife = maxLife;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getMaxStamina() {
+        return maxStamina;
+    }
+
+    public void setMaxStamina(int maxStamina) {
+        this.maxStamina = maxStamina;
+    }
+}
