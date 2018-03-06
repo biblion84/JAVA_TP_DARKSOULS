bcf4c0dff3bd5d4c4ffab76190ea05a08ef640db
Unknown <oblipn@gmail.com>
2018-03-06 17:11:37 +0100

    3.1 pas fini ( a setArmorItem )

--- src/characters/Hero.java
+++ src/characters/Hero.java
@@ -1,7 +1,13 @@
 package characters;
 
+import lsg.armor.ArmorItem;
+
+import java.util.ArrayList;
+
 public class Hero  extends Character{
 
+    final private static int MAX_ARMOR_PIECES = 3;
+    private ArrayList<ArmorItem> armor = new ArrayList<ArmorItem>(MAX_ARMOR_PIECES);
 
     public Hero(String name) {
         this.name = name;
@@ -13,6 +19,12 @@ public class Hero  extends Character{
 
     public Hero() {
         this("Ynovator");
+    }
+
+    public void setArmorItem(ArmorItem armorPiece, int slot) {
+        if (slot >= 0 && slot < MAX_ARMOR_PIECES){
+            this.armor.set(slot, armorPiece);
+        }
     }
 
 }
