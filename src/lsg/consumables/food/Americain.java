e03fbbbb6fc87a2da0927d313de3865ea59332fe
Unknown <oblipn@gmail.com>
2018-04-10 10:11:45 +0200

    2.3

---
+++ src/lsg/consumables/food/Americain.java
@@ -0,0 +1,7 @@
+package lsg.consumables.food;
+
+public class Americain extends Food {
+    public Americain() {
+        super("Uncle Greg's spicy Maroilles burger", 40);
+    }
+}
---
+++ src/lsg/consumables/food/Hamburger.java
@@ -0,0 +1,7 @@
+package lsg.consumables.food;
+
+public class Hamburger extends Food {
+    public Hamburger() {
+        super("Friterie 2000's Best of the Best", 3000);
+    }
+}
